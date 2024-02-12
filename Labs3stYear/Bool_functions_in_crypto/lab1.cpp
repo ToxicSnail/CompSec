54e5582913e080e32c3fdf19c903d864632d76ce
Gorky Kirill <59337566+ToxicSnail@users.noreply.github.com>
2024-02-12 12:52:28 +0300

    Create lab1.cpp

---
+++ Labs3stYear/Bool_functions_in_crypto/lab1.cpp
@@ -0,0 +1,20 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+
+class BF
+{
+public:
+        BF(int n, int type);    //type = {0,1,2}, if type = 0, then f = 0. type =1, f =1. type 2 - random
+        
+
+private:
+        int A, nw;
+        unsigned int *f;
+}
+
+int main() {
+    // Write C++ code here
+    std::cout << "Hello world!";
+
+    return 0;
+}
